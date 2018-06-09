58ed7ce221025b170a9c3e1955df87ebc7e68739
Md.Minhajul Islam <kmsohelinfo@gmail.com>
2018-06-09 16:31:53 +0600

    #7: Added isEmpty method to check empty stack

--- data-structures/stack/stack.js
+++ data-structures/stack/stack.js
@@ -13,19 +13,28 @@ class Stack {
             return -1;
         }
 
-        return this.store[--this.top];
+        return this.store.slice(--this.top, 1);
     }
 
     peak() {
+        if (this.isEmpty()){
+           return -1;
+        }
+
         return this.store[this.top - 1];
+    }
+
+    isEmpty(){
+        return this.top === 0;
+    }
+
+    getStack(){
+        console.log(this.store, 1);
+        return this.store;
     }
 
     length() {
        return this.top;
-    }
-
-    clear() {
-       this.top = 0
     }
 
 }
@@ -34,10 +43,10 @@ const stack = new Stack();
 stack.push('A');
 stack.push('B');
 stack.push('C');
-console.log(stack.store);
+console.log(stack.getStack());
 stack.pop();
-console.log(stack.length());
+console.log(stack.getStack());
 console.log(stack.peak());
-console.log(stack.clear());
+console.log(stack.length());
 
 
