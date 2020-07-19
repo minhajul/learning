b35254c0d8e963c8a42fb8ac5091ab4d8a6b8efc
minhajul <kmsohelinfo@gmail.com>
2020-07-19 10:46:53 +0600

    Updated

--- python/algorithms/linear_search.py
+++ python/algorithms/linear_search.py
@@ -1,8 +1,9 @@
 def linear_search(lists, x):
-	for i in range(len(lists)):
-		if lists[i] == x:
-			return i
-	return -1
+    for i in range(len(lists)):
+        if lists[i] == x:
+            return i
+    return -1
+
 
 lists = [1, 4, 3, 6, 8, 7, 2, 5]
 print(linear_search(lists, 3))
