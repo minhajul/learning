b3b7bc780816f5045880289592135a3cee871aad
minhajul <kmsohelinfo@gmail.com>
2020-07-26 12:20:22 +0600

    WIP - Updated

--- go/pointers.go
+++ go/pointers.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Post struct {
 	title       string
@@ -12,8 +14,30 @@ func main() {
 	post := &Post{title: "This is dummy post", author: "Minhajul"}
 	makePublish(post)
 	fmt.Println(post)
+
+	posts_array := make([]Post, 2)
+	posts_array = append(posts_array, Post{
+		title: "This is dummy post", author: "Minhajul", isPublished: true,
+	})
+
+	posts_array = append(posts_array, Post{
+		title: "This is dummy post", author: "Minhajul", isPublished: false,
+	})
+
+	fmt.Println(getPublishedPost(posts_array))
 }
 
 func makePublish(p *Post) {
 	p.isPublished = true
+}
+
+func getPublishedPost(posts []Post) []string {
+	posts := make([]string, 2)
+	for index, post := range posts {
+		if post.isPublished {
+			posts[index] = post.title
+		}
+	}
+
+	return posts
 }
